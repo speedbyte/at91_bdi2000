1bcc129f1d4fe6eb0e4d4e344c1c917e200f3ebf
Ykus <ykus.freimeister@gmail.com>
2015-12-01 20:02:47 +0100

    adding bootloader and few pdfs

---
+++ sourcecode/bootloader/messages.mk
@@ -0,0 +1,41 @@
+# Define programs and commands.
+
+TOOLCHAIN = linux
+# Define programs and commands.
+SHELL 	= sh
+CC 		= arm-$(TOOLCHAIN)-gcc
+OBJCOPY = arm-$(TOOLCHAIN)-objcopy
+OBJDUMP = arm-$(TOOLCHAIN)-objdump
+SIZE 	= arm-$(TOOLCHAIN)-size
+NM 		= arm-$(TOOLCHAIN)-nm
+REMOVE 	= rm -f
+COPY 	= cp
+
+
+
+makevariables:
+	@echo The Makefile inbuilt Variables
+	@echo MAKEFILES = $(MAKEFILES)
+	@echo MAKEFILE_LIST = $(MAKEFILE_LIST)
+	@echo .DEFAULT_GOAL = $(.DEFAULT_GOAL)
+	@echo MAKE_RESTARTS = $(MAKE_RESTARTS)
+	@echo .FEATURES = $(.FEATURES)
+	@echo .INCLUDE_DIRS = $(.INCLUDE_DIRS)
+.PHONY : .VARIABLES
+	@echo .VARIABLES = $(.VARIABLES)
+	
+# Define Messages
+
+MSG_ERRORS_NONE 		= Errors: none
+MSG_BEGIN 				= "------- begin (mode: $(RUN_MODE)) --------"
+MSG_END 				= --------  end  --------
+MSG_SIZE_BEFORE 		= Size before: 
+MSG_SIZE_AFTER 			= Size after:
+MSG_FLASH 				= Preparing load file for Flash:
+MSG_EXTENDED_LISTING 	= Creating Extended Listing:
+MSG_SYMBOL_TABLE 		= Creating Symbol Table:
+MSG_LINKING 			= Linking:
+MSG_COMPILING_ARM 		= "Compiling C (ARM-only):"
+MSG_ASSEMBLING_ARM 		= "Assembling (ARM-only):"
+MSG_CLEANING 			= Cleaning project:
+
